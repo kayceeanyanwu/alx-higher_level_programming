e2089eae2a44a2c63af72baabb4d5fee2dcfa357
Kelechi Anyanwu <kayceeanyanwu@gmail.com>
2022-10-03 16:33:34 +0000

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-'use strict';
-let l = process.argv.length;
-if (l === 2) {
+
+if (process.argv.length < 3) {
   console.log('No argument');
-} else if (l === 3) {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
