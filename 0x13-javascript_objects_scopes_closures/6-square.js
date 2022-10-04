f277356193cd0e8d10d64c97272bdc1899a75ad5
Kelechi Anyanwu <kayceeanyanwu@gmail.com>
2022-10-04 16:39:42 +0000

    6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const Father = require('./5-square');
+
+module.exports = class Square extends Father {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+};
