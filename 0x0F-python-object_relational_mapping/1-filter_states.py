995e77ec5b825c450cade56bffcdb1860b2b77ca
Kelechi Anyanwu <kayceeanyanwu@gmail.com>
2022-10-09 12:08:25 +0000

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,8 +10,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE states.name\
-                LIKE 'N%' ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY states.id ASC")
     records = cur.fetchall()
     for record in records:
         print(record)
