7937395e13423b6f95e6a0545235535ad8bed468
Kelechi Anyanwu <kayceeanyanwu@gmail.com>
2022-10-23 01:05:54 +0000

    5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# sends a POST request to the passed URL, and displays the body of the response
-curl -s "$1" -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD"
-
+# takes in a URL, sends a POST request to the passed URL, and displays the body of the response. A variable email must be sent with the value hr@holbertonschool.com. A variable subject must be sent with the value I will always be here for PLD
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
